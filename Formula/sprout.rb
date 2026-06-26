ae38267dd71c20becfcf575db08dd027e7a2372f
tanodev0 <alejandrohigueramoreno@gmail.com>
2026-06-27 00:53:47 +0200

    sprout 2.1.0: update release checksum

--- Formula/sprout.rb
+++ Formula/sprout.rb
@@ -2,7 +2,7 @@ class Sprout < Formula
   desc "Grow a ready-to-run project and open it in your editor"
   homepage "https://github.com/tanodev0/sprout"
   url "https://github.com/tanodev0/sprout/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "53486e1ce491f0c5d534719cad382479bf783854dd1c22c48810e1970dbdac3d"
+  sha256 "d534b40c4c0c825484af11431e18217b4c805bc4eb6156c7a8599a54bf5f2f10"
   license "MIT"
 
   def install
