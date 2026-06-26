9656d7100146611338d4916d708c1103eaaa5733
tanodev0 <alejandrohigueramoreno@gmail.com>
2026-06-27 00:45:30 +0200

    sprout 2.1.0

--- Formula/sprout.rb
+++ Formula/sprout.rb
@@ -1,8 +1,8 @@
 class Sprout < Formula
   desc "Grow a ready-to-run project and open it in your editor"
   homepage "https://github.com/tanodev0/sprout"
-  url "https://github.com/tanodev0/sprout/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "8a9f91ea4ae6b5af8786b553ac212dfaf90e069bc95e43cfdf99879002df69b0"
+  url "https://github.com/tanodev0/sprout/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "53486e1ce491f0c5d534719cad382479bf783854dd1c22c48810e1970dbdac3d"
   license "MIT"
 
   def install
@@ -10,6 +10,6 @@ class Sprout < Formula
   end
 
   test do
-    assert_match "sprout 2.0.0", shell_output("#{bin}/sprout --version")
+    assert_match "sprout 2.1.0", shell_output("#{bin}/sprout --version")
   end
 end
